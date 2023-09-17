cfb8a749c2da887e1eec3d5c4cc88a93b7b4066b
Ken Bima Satria Gandasasmita <kenbima73@gmail.com>
2023-09-17 16:27:43 +0700

    latihan git clone dan remote

---
+++ latihanarray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+   int a[3]={1,2,3};
+
+   for (int i = 0; i < 3; i++)
+   {
+    cout<<a[i]<<endl;
+   }
+   
+    return 0;
+}
